07710b7cc15e452d2f407783c5d39527a7928f78
supercaracal <supercaracal@yahoo.co.jp>
2012-04-24 09:47:24 +0900

    windows env

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 var LISTEN_PORT = 3000;
 var SERVER_URI = '127.0.0.1';
-var LIB_PATH = 'C:/Program Files/nodejs/node_modules/';
+var LIB_PATH = '';
 
-var app = require(LIB_PATH + 'express').createServer();
+var app;
+
+try {
+    app = require(LIB_PATH + 'express').createServer();
+} catch(ex) {
+    LIB_PATH = 'C:/Program Files/nodejs/node_modules/';
+    try {
+        app = require(LIB_PATH + 'express').createServer();
+    } catch(ex2) {
+        LIB_PATH = 'C:/Program Files (x86)/nodejs/node_modules/';
+        app = require(LIB_PATH + 'express').createServer();
+    }
+}
+
 var io = require(LIB_PATH + 'socket.io').listen(app);
 app.listen(LISTEN_PORT, SERVER_URI);
 
