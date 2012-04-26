c03d899dc84354af12e0b78c9ebd714ca26afddb
supercaracal <supercaracal@yahoo.co.jp>
2012-04-26 23:22:58 +0900

    response code

--- web.js
+++ web.js
@@ -15,9 +15,11 @@ try {
 app.listen(process.env.PORT || 3000);
 
 app.get('/', function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
     res.sendfile(__dirname + '/index.html');
 });
 app.get('/*', function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
     res.sendfile(__dirname + req.url);
 });
 
