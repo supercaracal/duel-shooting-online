5172b16057ae4d14d88c4c6b8fc235241f560ee4
supercaracal <supercaracal@yahoo.co.jp>
2012-05-30 13:02:00 +0900

    temporary commit

--- test/bullet-bezier.js
+++ test/bullet-bezier.js
@@ -1,9 +1,10 @@
 var BulletBezier = Class.create(Bullet, {
 
-    ATTAINABLE_COUNT: 100,
+    ATTAINABLE_COUNT: 500,
     pos: null,
     xRange: null,
     yRange: null,
+    count: null,
 
     initialize: function($super, ship, enemy) {
         $super(ship, enemy);
@@ -13,6 +14,7 @@ var BulletBezier = Class.create(Bullet, {
         };
         this.xRange = -(this.pos.ship.left - this.pos.enemy.left) / this.ATTAINABLE_COUNT;
         this.yRange = (this.pos.ship.top - this.pos.enemy.top) / this.ATTAINABLE_COUNT;
+        this.count = 0;
     },
 
     getColor: function() {
@@ -20,12 +22,18 @@ var BulletBezier = Class.create(Bullet, {
     },
 
     move: function() { // TODO
+        ++this.count;
         var range = this.isFall ? this.yRange : -this.yRange;
         if (this.enemy.isHit(this, range)) {
             this.isDelete = true;
             this.elm.remove();
             return;
         }
-        this.setPos({top: this.getTop() + range, left: this.getLeft() + this.xRange});
+        var x = this.pos.ship.left + this.xRange * this.count;
+        var y = this.pos.ship.top + range * this.count;
+        this.setPos({
+            top: y - y * this.count / this.ATTAINABLE_COUNT,
+            left: x + (this.pos.enemy.left - x) * this.count / this.ATTAINABLE_COUNT
+        });
     }
 });
